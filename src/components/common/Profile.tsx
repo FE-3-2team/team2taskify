f29502cdab81647ecbc9fb066bf5e1caf604a22b
nerte <grimza@nerteui-MacBookPro.local>
2025-03-16 03:03:43 +0900

    [Style] Profil 반응형, 컬러 적용

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -7,7 +7,9 @@ export default function Profile({ value }: Props) {
   return (
     <div className="flex flex-row items-center gap-3">
       <Badge value={value} />
-      <p className="text-base">{value?.nickname}</p>
+      <p className="hidden text-lg-medium text-black-333236 tablet:block">
+        {value?.nickname}
+      </p>
     </div>
   );
 }
