eeac05594e8f943dac67777eb71d91c625c3c8b0
nerte <grimza@nerteui-MacBookPro.local>
2025-03-15 15:56:15 +0900

    [Feat] create chip & random tags

---
+++ src/components/common/Chip/Status.chip.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+interface Props {
+  value: string;
+}
+/**
+ * todo
+ * 점 , 배경색 컬러변경, 글씨 색깔, font-
+ */
+
+export default function Status({ value }: Props) {
+  return (
+    <div className=" flex items-center w-fit min-h-[26px] bg-purple-200 rounded-2xl text-[14px] px-2 ">
+      <div className="gap-[6px] h-fit flex items-center text-violet-500">
+        <div className="w-[6px] h-[6px] bg-violet-500  rounded-full" />
+        {value}
+      </div>
+    </div>
+  );
+}
