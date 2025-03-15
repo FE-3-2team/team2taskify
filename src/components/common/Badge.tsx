42721202bc0eda80fe01da30a4089a44db840015
nerte <grimza@nerteui-MacBookPro.local>
2025-03-15 19:52:28 +0900

    [Feat] create memberCountBadge and random -> contants

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
-interface Props {
-  value: Member;
+interface BadgesProps {
+  badges: Member[];
 }
 
-const randomBg = [
+const RANDOM_COLOR = [
   //여기 컬러 변경 예정
   "bg-red-200",
   "bg-red-300",
@@ -15,33 +13,35 @@ const randomBg = [
   "bg-red-50",
   "bg-red-700",
 ];
+const RANDOM_NUM = Math.floor(Math.random() * RANDOM_COLOR.length);
+
+export function Badges({ badges }: BadgesProps) {
+  return (
+    <div className="">
+      {badges.map((badge) => {
+        return <Badge value={badge}></Badge>;
+      })}
+      <div className={`${RANDOM_COLOR[RANDOM_NUM]}`}>{badges.length - 3}</div>
+    </div>
+  );
+}
+interface Props {
+  value: Member;
+}
+
 export function Badge({ value }: Props) {
   const { nickname, profileImageUrl } = value;
   const firstChar = nickname.charAt(0).toUpperCase();
-  const randomColor = Math.floor(Math.random() * randomBg.length);
 
   return (
     <div
-      className={` border-2 border-white  flex justify-center items-center rounded-full w-[38px] h-[38px] ${randomBg[randomColor]} `}
+      className={` border-2 border-white  flex justify-center items-center rounded-full w-[38px] h-[38px] ${RANDOM_COLOR[RANDOM_NUM]} `}
     >
       {profileImageUrl ? (
         <img src={profileImageUrl} alt="프로필 이미지" />
       ) : (
         <div className="text-white text-[16px]">{firstChar}</div> //여기 font,color 바꿔줘야함
       )}
-    </div>
-  );
-}
-
-interface BadgesProps {
-  badges: Member[];
-}
-export function Badges({ badges }: BadgesProps) {
-  return (
-    <div>
-      {badges.map((badge) => {
-        return <Badge value={badge}></Badge>;
-      })}
     </div>
   );
 }
