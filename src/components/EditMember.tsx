6e5c0768a9ab81e6c19665c9749af8f1f76e2247
nerte <grimza@nerteui-MacBookPro.local>
2025-03-17 04:32:00 +0900

    [Feat] create EditMember

---
+++ src/components/EditMember.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import PrevPage from "@/assets/icons/PrevPage.icon.svg";
+import NextPage from "@/assets/icons/NextPage.icon.svg";
+import Image from "next/image";
+import Profile from "./common/Profile";
+//
+interface Props {
+  members: User[];
+}
+/**ToDo
+ * 삭제 버튼 공통컴포넌트로 바꾸기
+ * 멤버 삭제 버튼클릭시 함수
+ */
+export default function EditMember({ members }: Props) {
+  const totalPage = Math.ceil(members.length / 4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const handelClickPrev = () => {
+    if (currentPage === 1) return;
+    setCurrentPage((prev) => prev - 1);
+  };
+  const handelClickNext = () => {
+    if (currentPage === totalPage) return;
+    setCurrentPage((prev) => prev + 1);
+  };
+  const handleClick = () => {
+    //member 삭제 함수
+  };
+  //
+  return (
+    <div
+      className="flex flex-col  max-w-[284px] max-h-[337px] tablet:max-w-[544px] tablet:max-h-[404px] laptop:max-w-[620px] laptop:max-h-[404px] 
+      pt-[22px] pb-4 tablet:pt-[26px] tablet:pb-5 gap-[13px]"
+    >
+      <div className="flex flex-col gap-[18px]">
+        <div className="flex items-center justify-between px-5 tablet:px-7 ">
+          <p className="text-xl-bold tablet:text-2xl-bold">구성원</p>
+          <div className="flex items-center gap-3 text-xs-regular tablet:text-md-regular">
+            {totalPage} 페이지 중 {currentPage}
+            <div>
+              <button className="cursor-pointer" onClick={handelClickPrev}>
+                <Image src={PrevPage} width={40} height={40} alt="<" />
+              </button>
+              <button className="cursor-pointer" onClick={handelClickNext}>
+                <Image src={NextPage} width={40} height={40} alt=">" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="px-5 tablet:px-7 md-regular text-gray-9FA6B2">이름</p>
+      </div>
+      <div>
+        {members.map((member, i) => {
+          return (
+            <div>
+              <div className="flex items-center justify-between px-5 tablet:px-7 h-[34px] tablet:h-[38px]">
+                <Profile value={member} />
+                <button onClick={handleClick}>삭제</button>
+              </div>
+              <div
+                className="h-[1px] w-full bg-gray-EEEEEE my-3"
+                style={{ display: i === 3 ? "none" : "block" }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,3 +1,5 @@
+import EditMember from "@/components/EditMember";
+
 const user = {
   id: 0,
   email: "gbtm@naver.com",
@@ -40,20 +42,13 @@ const members = [
     createdAt: "string",
     updatedAt: "string",
   },
-  {
-    id: 0,
-    email: "gbtm@naver.com",
-    nickname: "유선향",
-    profileImageUrl: null,
-    createdAt: "string",
-    updatedAt: "string",
-  },
 ];
 
 export default function Test() {
   return (
-    <div>
+    <div className="h-dvw">
       <div>컴포넌트등 테스트 페이지 입니다.</div>
+      <EditMember members={members} />
     </div>
   );
 }
