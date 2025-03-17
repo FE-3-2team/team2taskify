4a27e4a83606804668612e74c40989176bd4cf0e
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-03-17 18:24:15 +0900

    [Feat] Create a Progress dropdown

--- src/components/common/Dropdown/DropdownEditDel.tsx
+++ src/components/common/Dropdown/DropdownEditDel.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
-import Kebab from "@/assets/icons/kebabmenu.icon.svg";
+import Kebab from "@/assets/icons/Kebabmenu.icon.svg";
 
 interface DropdownEditDelProps {
   onEdit: () => void;
@@ -16,16 +16,16 @@ const DropdownEditDel: React.FC<DropdownEditDelProps> = ({
   return (
     <div className="relative w-[28px] h-[28px] flex items-center justify-center">
       <button
-        className="w-full h-full flex items-center justify-center text-gray-600 hover:bg-gray-100 rounded-full"
+        className="flex items-center justify-center w-full h-full text-gray-600 rounded-full hover:bg-gray-100"
         onClick={() => setIsOpen(!isOpen)}
       >
         <Image src={Kebab} alt="menu" className="h-full" />
       </button>
 
       {isOpen && (
-        <div className="absolute right-0 top-full mt-1 w-[93px] h-[82px] bg-white shadow-lg rounded-[6px] p-[6px] border border-gray-200 text-sm z-50">
+        <div className="absolute right-0 top-full mt-1 w-[93px] h-[82px] bg-white shadow-md rounded-[6px] p-[6px] border border-gray-200 text-sm-medium z-50">
           <button
-            className="w-full text-center text-[#333236] hover:text-[#5534DA] hover:bg-[#F1EFFD] rounded px-2 py-2"
+            className="w-full h-[32px] mb-[4px] text-center text-black-333236 hover:text-[#5534DA] hover:bg-[#F1EFFD] rounded px-2 py-2"
             onClick={() => {
               onEdit();
               setIsOpen(false);
@@ -34,7 +34,7 @@ const DropdownEditDel: React.FC<DropdownEditDelProps> = ({
             수정하기
           </button>
           <button
-            className="w-full text-center text-[#333236] hover:text-[#5534DA] hover:bg-[#F1EFFD] rounded px-2 py-2"
+            className="w-full h-[32px] text-center text-[#333236] hover:text-[#5534DA] hover:bg-[#F1EFFD] rounded px-2 py-2"
             onClick={() => {
               onDelete();
               setIsOpen(false);
