3a4be5a1d54146e70b4ae0f76fe0210cfcc43664
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-03-17 22:44:16 +0900

    [Feat] Add title of the progress dropdown

--- src/components/common/Dropdown/DropdownProgress.tsx
+++ src/components/common/Dropdown/DropdownProgress.tsx
@@ -34,36 +34,39 @@ const DropdownProgress: React.FC<DropdownProgressProps> = ({
   }, []);
 
   return (
-    <div className="relative w-[180px]" ref={dropdownRef}>
-      <button
-        className="flex items-center justify-between w-full h-[48px] px-3 py-2 bg-white border border-gray-300 rounded-lg"
-        onClick={() => setIsOpen((prev) => !prev)}
-      >
-        <Status value={selectedTitle} />
-        <Image src={DownIcon} alt="" width={26} height={26} />
-      </button>
+    <div>
+      <div className="font-bold text-lg-regular mb-[10px]">상태</div>
+      <div className="relative w-[180px]" ref={dropdownRef}>
+        <button
+          className="flex items-center justify-between w-full h-[48px] px-3 py-2 bg-white border border-gray-300 rounded-lg"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <Status value={selectedTitle} />
+          <Image src={DownIcon} alt="" width={26} height={26} />
+        </button>
 
-      {isOpen && (
-        <div className="absolute left-0 w-full h-fit mt-[2px] bg-white border border-gray-300 rounded-md shadow-md top-full z-50">
-          {options.map((title) => (
-            <button
-              key={title}
-              className={
-                "flex items-center px-[16px] py-[8px] w-full h-[48px] text-left hover:bg-gray-100"
-              }
-              onClick={() => {
-                setIsOpen(false);
-                onChange(title);
-              }}
-            >
-              <div className="w-[30px]">
-                {title === selectedTitle && <Image src={CheckIcon} alt="" />}
-              </div>
-              <Status value={title} />
-            </button>
-          ))}
-        </div>
-      )}
+        {isOpen && (
+          <div className="absolute left-0 w-full h-fit mt-[2px] bg-white border border-gray-300 rounded-md shadow-md top-full z-50">
+            {options.map((title) => (
+              <button
+                key={title}
+                className={
+                  "flex items-center px-[16px] py-[8px] w-full h-[48px] text-left hover:bg-gray-100"
+                }
+                onClick={() => {
+                  setIsOpen(false);
+                  onChange(title);
+                }}
+              >
+                <div className="w-[30px]">
+                  {title === selectedTitle && <Image src={CheckIcon} alt="" />}
+                </div>
+                <Status value={title} />
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
