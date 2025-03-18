89fe36d027ec6b42bf710be2a595508ac5c42c23
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-03-18 22:25:41 +0900

    [Feat] Add responsive styles to AssigneeCard

--- src/components/AssigneeCard.tsx
+++ src/components/AssigneeCard.tsx
@@ -14,12 +14,12 @@ const AssigneeCard: React.FC<AssigneeCardProps> = ({
   const hasDueDate = dueDate ? dueDate : "마감일 없음";
 
   return (
-    <div className="w-[200px] h-[155px] bg-white rounded-[8px] p-4 border border-[#d9d9d9]">
-      <div className="mb-3">
-        <h3 className="text-sm text-gray-500">담당자</h3>
-        <div className="flex items-center mt-1 space-x-3">
+    <div className="tablet:w-[200px] tablet:h-[155px] bg-white rounded-[8px] px-[16px] tablet:py-[13px] py-[6px] border border-[#d9d9d9] flex flex-row tablet:flex-col justify-between w-[295px] h-[64px]">
+      <div className="w-fit tablet:h-[60px] h-[46px] flex flex-col justify-between">
+        <h3 className="text-black-000000 text-xs-semibold">담당자</h3>
+        <div className="flex items-center justify-start w-fit h-[34px] gap-[8px]">
           <div
-            className={`w-10 h-10 flex items-center justify-center rounded-full text-sm font-bold text-white ${
+            className={`tablet:w-[34px] tablet:h-[34px] w-[26px] h-[26px] flex items-center justify-center rounded-full border-[2px] border-white text-lg-semibold text-white ${
               hasProfileImage ? "" : "bg-green-500"
             }`}
             style={
@@ -34,13 +34,17 @@ const AssigneeCard: React.FC<AssigneeCardProps> = ({
           >
             {!hasProfileImage && initial}
           </div>
-          <span className="font-medium text-gray-800">{assigneeName}</span>
+          <span className="text-black-333236 tablet:text-md-regular text-xs-regular">
+            {assigneeName}
+          </span>
         </div>
       </div>
 
-      <div>
-        <h3 className="text-sm text-gray-500">마감일</h3>
-        <p className="mt-1 font-medium text-gray-800">{hasDueDate}</p>
+      <div className="w-fit tablet:h-[50px] h-[46px] flex flex-col justify-between">
+        <h3 className="text-black-000000 text-xs-semibold">마감일</h3>
+        <p className="tablet:text-md-regular text-xs-regular text-black-333236">
+          {hasDueDate}
+        </p>
       </div>
     </div>
   );
