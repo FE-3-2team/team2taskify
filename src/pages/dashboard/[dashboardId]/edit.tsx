06e9a985b0b59e51e9f770db70ddbf19bc0f755d
nerte <grimza@nerteui-MacBookPro.local>
2025-03-22 03:00:07 +0900

    [Refactor] client 컴포넌트로 변경

--- src/pages/dashboard/[dashboardId]/edit.tsx
+++ src/pages/dashboard/[dashboardId]/edit.tsx
@@ -1,72 +1,36 @@
+"use client";
 import Header from "@/components/common/Header";
 import EditMember from "@/components/EditMember";
 import InvitationHistory from "@/components/InvitationHistory";
-import NewDashboard from "@/components/ModalContents/NewDashboard";
 import { getMember } from "@/api/member";
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { getInvitations } from "@/api/invitations.api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DashButton } from "@/components/common/Button";
 import { deleteDashboard } from "@/api/dashboard";
 import arrow from "@/assets/icons/LeftArrow.icon.svg";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import EditDashboard from "@/components/ModalContents/EditDashboard";
+import { useParams } from "next/navigation";
 //
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [{ params: { dashboardId: "13624" } }],
-    fallback: true,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (
-  context: GetStaticPropsContext
-) => {
-  let cursorId;
-  let initialMembers;
-  let initialInvitations;
-  let result;
-  const { dashboardId } = context.params!;
-  try {
-    const res = await getMember(Number(dashboardId));
-    const invitationData = await getInvitations((cursorId = 0));
-    initialMembers = res.data.results ?? [];
-
-    initialMembers = res?.data?.results ?? []; // undefined 방지
-    initialInvitations = invitationData?.invitations ?? []; // undefined 방지
-
-    cursorId = invitationData?.cursorId ?? 0; // null 방지
-  } catch {
-    result = { initialInvitations: [], initialMembers: [] };
-  }
-  console.log(initialInvitations);
-  console.log(initialMembers);
-  console.log(dashboardId);
-
-  return {
-    props: {
-      initialMembers: initialMembers ?? [],
-      initialInvitations: initialInvitations ?? [],
-      dashboardId,
-    },
-  };
-};
-
-interface Props {
-  initialMembers: Member[];
-  initialInvitations: Invitation[];
-  dashboardId: string;
-}
-export default function EditPage({
-  initialMembers,
-  initialInvitations,
-  dashboardId,
-}: Props) {
+export default function EditPage() {
   const router = useRouter();
-  const [members, setMembers] = useState(initialMembers);
-  const [invitations, setInvitations] = useState(initialInvitations);
+  const { dashboardId } = useParams();
+  const [members, setMembers] = useState([]);
+  const [invitations, setInvitations] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  let pageCount;
+  const handleLoad = async () => {
+    const members = await getMember(Number(dashboardId));
+    const { totalCount, invitations } = await getInvitations(currentPage + 1);
+    setMembers(members);
+    setInvitations(invitations);
+    pageCount = totalCount;
+  };
 
+  useEffect(() => {
+    handleLoad();
+  }, []);
   const DashBoardDelete = async () => {
     await deleteDashboard(Number(dashboardId));
     router.push("/mypage");
@@ -84,9 +48,9 @@ export default function EditPage({
 
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-4">
-              <NewDashboard />
+              <EditDashboard title={} color={}></EditDashboard>
               <EditMember members={members} />
-              <InvitationHistory invitations={invitations} />
+              <InvitationHistory count={pageCount} invitations={invitations} />
             </div>
             <div className=" tablet:w-[320px] h-[52px] tablet:h-[62px] ">
               <DashButton onClick={DashBoardDelete} size="medium">
