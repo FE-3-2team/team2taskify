3083b2f778adce00bff94f756d47701c1c208199
nerte <grimza@nerteui-MacBookPro.local>
2025-03-21 22:28:12 +0900

    [Chore]delete test content

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -41,42 +41,6 @@ export default function Test() {
           이버튼을 누르면 accessToken을 받습니다.
         </button>
         <button onClick={TestLogout}>로그아웃</button>
-        <Modal
-          rightOnClick={() => {}}
-          ModalCloseButton="취소"
-          ModalOpenButton="기본 모달버튼 "
-          rightHandlerText="생성"
-        >
-          <div>여기가 모달의 내용입니다 이안에 모달 컨텐츠를 넣어야 해요.</div>
-        </Modal>
-        <Modal
-          rightOnClick={() => {}}
-          ModalCloseButton="취소"
-          ModalOpenButton="기본 모달버튼 "
-          rightHandlerText="오른쪽"
-          leftOnClick={() => {}}
-          leftHandlerText="왼쪽"
-        >
-          <div>leftHandlerText와 leftOnClick을 넣어야 해요</div>
-        </Modal>
-        <Modal
-          rightOnClick={() => {}}
-          ModalCloseButton="취소"
-          ModalOpenButton={
-            <div className="w-[200px]">이렇게 사용하셔야 해요</div>
-          }
-          rightHandlerText="오른쪽"
-          leftOnClick={() => {}}
-          leftHandlerText="왼쪽"
-        >
-          <div>
-            여기는 핸들러가 두개인 모달입니다. leftHandlerText와 leftOnClick을
-            넣어야 해요
-          </div>
-        </Modal>
-        <DetailContent ModalOpenButton="상세보기 버튼">
-          <CardModal card={card} columnTitle="테스트" comments={[]} />
-        </DetailContent>
       </div>
     </div>
   );
