a9347e9cbd68a56f2fab32aa03a0dbf17d5a384b
nerte <grimza@nerteui-MacBookPro.local>
2025-03-24 01:14:23 +0900

    [Feat] mydashboardPage 작성

--- src/pages/mydashboard.tsx
+++ src/pages/mydashboard.tsx
@@ -1,10 +1,94 @@
+import { createDashboard, getDashboards } from "@/api/dashboard";
 import Header from "@/components/common/Header";
+import { Modal } from "@/components/common/ModalPopup";
+import NewDashboard from "@/components/ModalContents/NewDashboard";
+import { useEffect, useState } from "react";
+import {
+  DashButton,
+  PaginationButton,
+  PlusIconButton,
+} from "@/components/common/Button";
+import InvitedDashboards from "@/components/common/InvitedDashboards/InvitedDashboards";
 
+export interface Data {
+  title: string;
+  color: string;
+}
 export default function MyDashboard() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [boardList, setBoardList] = useState<Dashboards[]>([]);
+  const [newColor, setNewColor] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+
+  useEffect(() => {
+    handleLoad();
+  }, [currentPage]);
+  //
+  const handleLoad = async () => {
+    const { totalCount, dashboards } = await getDashboards(currentPage);
+    setBoardList(dashboards);
+    setTotalPage(Math.ceil(totalCount / 5));
+  };
+  const plusDashboard = async () => {
+    await createDashboard(newTitle, newColor);
+  };
+  const handleChange = (title: string, color: string) => {
+    setNewColor(color);
+    setNewTitle(title);
+  };
+
+  const NextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+  const PrevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
   return (
-    <div>
+    <div className="ml-[67px] tablet:ml-[160px] laptop:ml-[300px]">
       <Header />
-      <div> 마이 대시보드 페이지</div>
+      <div className="flex flex-col py-6 px-6 tablet:py-10 tablet:px-10 gap-6 tablet:gap-12 laptop:gap-10 max-w-[1022px]">
+        <div>
+          <div className="w-full grid gap-[10px] grid-cols-1 tablet:grid-cols-2  laptop:grid-cols-3">
+            <div className="w-full h-full">
+              <Modal
+                ModalOpenButton={
+                  <div className="flex gap-3">
+                    <p className="text-lg-semibold tablet:text-lg-semibold">
+                      새로운 대시보드 만들기
+                    </p>
+                    <PlusIconButton />
+                  </div>
+                }
+                rightHandlerText="생성"
+                rightOnClick={plusDashboard}
+                variant="create"
+              >
+                <NewDashboard onChange={handleChange} />
+              </Modal>
+            </div>
+            {boardList.map((board) => {
+              return (
+                <DashButton hasArrow title={board.title} color={board.color} />
+              );
+            })}
+          </div>
+          <div className="flex items-center justify-end gap-4">
+            <p>
+              {totalPage} 중 {currentPage}
+            </p>
+            <PaginationButton
+              hasPrev={currentPage > 1}
+              hasNext={totalPage > currentPage}
+              onPrev={PrevPage}
+              onNext={NextPage}
+            />
+          </div>
+        </div>
+
+        <InvitedDashboards />
+      </div>
     </div>
   );
 }
