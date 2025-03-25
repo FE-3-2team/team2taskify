ee4898e48cf35ff744bd1f18e713644e9e176467
eplssun <pretty_who@naver.com>
2025-03-25 18:16:34 +0900

    [Feat] 랜딩페이지전체완료

--- src/components/common/MainHeader.tsx
+++ src/components/common/MainHeader.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import LogoImage from '@/assets/icons/LogoImage.svg';
-import LogoFull from '@/assets/icons/LogoFull.svg';
-import LogoImageDark from '@/assets/icons/logoimage_dark.svg';
-import LogoFullDark from '@/assets/icons/logofull_dark.svg';
+import LogoImage from '@/assets/LogoImage.svg';
+import LogoFull from '@/assets/LogoFull.svg';
+import LogoImageDark from '@/assets/logoimage_dark.svg';
+import LogoFullDark from '@/assets/logofull_dark.svg';
 
 export default function MainHeader() {
 
   return (
-    <header className='m-auto flex items-center justify-between px-[24px] py-[16px] tablet:px-[40px] laptop:px-[80px] dark:bg-black-100 dark:text-white'>
+    <header className='m-auto flex items-center justify-between px-[24px] py-[16px] tablet:px-[40px] laptop:px-[80px] dark:bg-black-400 dark:text-white'>
       <Link href='/' className='dark:hidden'>
         <Image src={LogoImage} alt='로고 아이콘' className='block tablet:hidden w-[24px]'/>
         <Image src={LogoFull} alt='텍스트 로고' className='hidden tablet:block w-[120px]'/>
@@ -18,7 +18,7 @@ export default function MainHeader() {
         <Image src={LogoFullDark} alt='텍스트 로고' width={100} height={40} className='hidden tablet:block w-[120px]'/>
       </Link>
 
-      <div className='flex gap-5 text-md-regular tablet:text-lg-regular'>
+      <div className='flex gap-[24px] tablet:gap-[36px] text-md-regular tablet:text-lg-regular'>
         <Link href='/login'>로그인</Link>
         <Link href='/signup'>회원가입</Link>
       </div>
