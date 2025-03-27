42ee7b7061016389fe905f86c838d5e196c62d21
doctor-taco <100111506+doctor-taco@users.noreply.github.com>
2025-03-28 00:55:36 +0900

    [Fix] Fix missing leftOnClick calling

--- src/components/common/ModalPopup.tsx
+++ src/components/common/ModalPopup.tsx
@@ -79,7 +79,7 @@ export function Modal({
                     variant="outline"
                     onClick={() => {
                       handleSetIsOpen(false);
-                      leftOnClick;
+                      leftOnClick();
                     }}
                   >
                     {leftHandlerText}
