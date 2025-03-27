bc497a547b780feeea265ba6aa87b29e39d04211
eplssun <pretty_who@naver.com>
2025-03-27 13:44:51 +0900

    [Feat]패스워드변경및마이페이지생성

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,10 +1,17 @@
 import Header from "@/components/common/Header";
+import SideMenu from "@/components/common/SideMenu";
+import ChangePasswordForm from "@/components/ChangePasswordForm";
 
 export default function MyPage() {
   return (
-    <div>
+    <>
+    <div className="ml-[67px] tablet:ml-[160px] laptop:ml-[300px]">
+      <SideMenu />
       <Header />
-      <div>마이 페이지</div>
+      <div className="flex flex-col py-6 px-6 tablet:py-10 tablet:px-10 gap-6 tablet:gap-12 laptop:gap-10 max-w-[1022px]">
+        <ChangePasswordForm />
+      </div>
     </div>
+    </>
   );
 }
