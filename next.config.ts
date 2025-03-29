e7d6c049ddcaebf514c80a0e20605a32eb9b30e3
eplssun <pretty_who@naver.com>
2025-03-29 12:54:05 +0900

    [Fix] config.js 문법수정

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
-
-export default nextConfig;
-
-module.exports = {
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
   },
 };
+
+module.exports = nextConfig;
