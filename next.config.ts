a84fd57df1e39f0144e2b36b07b4520c6bd3f4f9
eplssun <pretty_who@naver.com>
2025-03-29 13:18:02 +0900

    [Fix] config.js 원복(profile페이지구현방식변경하기로)

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
