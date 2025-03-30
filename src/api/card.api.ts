bc0ef2e8b2b6215b1e07d1fd5476e4edcd33e28f
nerte <grimza@nerteui-MacBookPro.local>
2025-03-31 01:19:11 +0900

    [Feat] 할일 수정, 할일 상세보기 에러 핸들링

--- src/api/card.api.ts
+++ src/api/card.api.ts
@@ -10,16 +10,12 @@ export async function getCardDetail(cardId: number) {
 }
 //카드 목록 조회
 export async function getCards(columnId: number): Promise<Card[]> {
-  try {
-    const res = await instance.get(`/cards`, {
-      params: { columnId },
-    });
+  const res = await instance.get(`/cards`, {
+    params: { columnId },
+  });
 
-    // console.log("getCards 응답 결과:", res.data);
-    return res.data.cards;
-  } catch (err) {
-    throw new Error("카드 목록 조회 실패");
-  }
+  // console.log("getCards 응답 결과:", res.data);
+  return res.data.cards;
 }
 
 //카드 삭제
--- src/api/column.api.ts
+++ src/api/column.api.ts
@@ -1,16 +1,11 @@
 import { instance } from "./instance";
 
 export async function getColumns(dashboardId: number): Promise<Column[]> {
-  try {
-    const res = await instance.get(`/columns`, {
-      params: { dashboardId },
-    });
-    // console.log("컬럼 응답 확인:", res.data);
-    return res.data.data;
-  } catch (error) {
-    console.error("getColumns error:", error);
-    throw new Error("컬럼 목록 조회 실패");
-  }
+  const res = await instance.get(`/columns`, {
+    params: { dashboardId },
+  });
+  // console.log("컬럼 응답 확인:", res.data);
+  return res.data.data;
 }
 
 export async function createColumn({
