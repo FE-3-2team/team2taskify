8a34921cf43df3124c1c94090030e106b33e8b59
eplssun <pretty_who@naver.com>
2025-03-31 15:32:06 +0900

    [Feat] FadeInWhenVisible.tsx 인덱스화면 모션컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import landing3 from '@/assets/landing3.png';
 import landing4 from '@/assets/landing4.png';
 import landing5 from '@/assets/landing5.png';
 import { Button } from "@/components/common/Button";
+import FadeInWhenVisible from "@/components/FadeInWhenVisible";
 
 export default function Home() {
   
@@ -19,6 +20,7 @@ export default function Home() {
       </header>
       <main className="dark">
         <div className="dark:bg-black-400 pt-[42px] text-center m-auto flex flex-col justify-center items-center">
+        <FadeInWhenVisible>
           {/* section01 */}
           <div className="flex flex-col justify-center items-center mb-[76px] tablet:mb-[180px]">
             <Image src={mainimage01} alt='taskify 메인이미지' className='w-full max-w-[288px] tablet:max-w-[540px] laptop:max-w-[720px] mb-[28px] tablet:mb-[48px]'/>
@@ -30,10 +32,12 @@ export default function Home() {
               <Button size="xlarge" variant="primary">로그인하기</Button>
             </Link>
           </div>
+        </FadeInWhenVisible>
 
           {/* section02 */}
-          <div className="flex flex-col items-center justify-center laptop:justify-between mb-[90px] tablet:mb-[180px] px-[16px] tablet:px-[0] w-full max-w-[1200px] gap-[60px] tablet:gap-[90px]">
+          <div className="flex flex-col items-center justify-center laptop:justify-between mb-[90px] tablet:mb-[180px] px-[16px] laptop:px-[0] w-full max-w-[1200px] gap-[60px] tablet:gap-[90px]">
             {/* point 1 */}
+            <FadeInWhenVisible>
             <div className='relative flex flex-col laptop:flex-row justify-center bg-black-300 rounded-[8px] w-full pt-[56px] tablet:pt-[64px] laptop:tablet:pt-[100px]'>
               <div className="flex flex-col tablet:text-left laptop:w-1/2 tablet:pl-[60px]">
                 <span className='text-2lg-medium tablet:text-[22px] text-gray-400 mb-[60px] tablet:mb-[100px] laptop:pt-[20px]'>Point 1</span>
@@ -43,7 +47,9 @@ export default function Home() {
                 <Image src={landing1} alt='taskify 대시보드 이미지 01' className='w-full max-w-[296px] tablet:max-w-[520px] laptop:max-w-[592px]'/>
               </div>
             </div>
+            </FadeInWhenVisible>
             {/* point 2 */}
+            <FadeInWhenVisible>
             <div className='relative flex flex-col laptop:flex-row laptop:flex-row-reverse justify-center bg-black-300 rounded-[8px] w-full pt-[56px] tablet:pt-[64px] laptop:tablet:pt-[100px]'>
               <div className="flex flex-col tablet:text-left laptop:w-1/2 tablet:pl-[60px] laptop:pl-[40px]">
                 <span className='text-2lg-medium tablet:text-[22px] text-gray-400 mb-[60px] tablet:mb-[100px] laptop:pt-[20px]'>Point 2</span>
@@ -53,12 +59,14 @@ export default function Home() {
                 <Image src={landing2} alt='taskify 대시보드 이미지 02' className='w-full max-w-[220px] tablet:max-w-[360px] laptop:max-w-[436px]'/>
               </div>
             </div>
+            </FadeInWhenVisible>
           </div>
           
           {/* section03 */}
           <div className="flex flex-col items-center justify-center w-full px-[16px] mb-[30] tablet:mb-[160px]">
             <strong className='mb-[40px] text-xl-bold text-white text-[22px] tablet:text-[28px]'>생산성을 높이는 다양한 설정 ⚡</strong>
             <ul className='w-full flex flex-col items-center laptop:flex-row laptop:justify-between gap-[33px] max-w-[1200px]'>
+            <FadeInWhenVisible>
               <li className='w-full tablet:max-w-[378px] laptop:max-w-full'>
                 <div className='flex justify-center items-center h-[236px] tablet:h-[260px] bg-black-100 rounded-t-[8px]'>
                   <Image src={landing3} alt='taskify 대시보드 이미지 03' className='w-full max-w-[260px] tablet:max-w-[300px]'/>
@@ -68,6 +76,8 @@ export default function Home() {
                   <p className='text-lg-medium'>대시보드 사진과 이름을 변경할 수 있어요.</p>
                 </div>
               </li>
+              </FadeInWhenVisible>
+              <FadeInWhenVisible>
               <li className='w-full tablet:max-w-[378px] laptop:max-w-full'>
                 <div className='flex justify-center items-center h-[236px] tablet:h-[260px] bg-black-100 rounded-t-[8px]'>
                   <Image src={landing4} alt='taskify 대시보드 이미지 03' className='w-full max-w-[260px] tablet:max-w-[300px]'/>
@@ -77,6 +87,8 @@ export default function Home() {
                   <p className='text-lg-medium'>새로운 팀원을 초대할 수 있어요.</p>
                 </div>
               </li>
+              </FadeInWhenVisible>
+              <FadeInWhenVisible>
               <li className='w-full tablet:max-w-[378px] laptop:max-w-full]'>
                 <div className='flex justify-center items-center h-[236px] tablet:h-[260px] bg-black-100 rounded-t-[8px]'>
                   <Image src={landing5} alt='taskify 대시보드 이미지 03' className='w-full max-w-[260px] tablet:max-w-[300px]'/>
@@ -86,6 +98,7 @@ export default function Home() {
                   <p className='text-lg-medium'>구성원을 초대하고 내보낼 수 있어요.</p>
                 </div>
               </li>
+              </FadeInWhenVisible>
             </ul>
           </div>
         
